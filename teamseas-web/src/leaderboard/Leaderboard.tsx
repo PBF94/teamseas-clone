c4188c585870e535bd27e7f5fbfb9e35ce396363
Bogdan Pop <bogdan.pop@hotmail.com>
2022-09-15 15:58:55 +0300

    feat(web): add donations real data and ordering

--- teamseas-web/src/leaderboard/Leaderboard.tsx
+++ teamseas-web/src/leaderboard/Leaderboard.tsx
@@ -1,42 +1,79 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Radio,
+  RadioGroup,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { DonationData } from "../types";
 import LeaderboardItem from "./LeaderboardItem";
+import { useQuery } from "urql";
 
-type Props = {};
+const DonationsQuery = `
+    query GetAllDonations($orderBy: OrderByParams) {
+        donations(orderBy: $orderBy) {
+            id
+            count
+            displayName
+            email
+            mobile
+            team
+            message
+            createdAt
+        }
+    }
+`;
+
+type DonationsQueryResponse = {
+  donations: DonationData[];
+};
+
+interface Props {}
 
 const Leaderboard = (props: Props) => {
+  const [field, setField] = useState<string>("createdAt");
+
+  const [{ data, fetching, error }] = useQuery<DonationsQueryResponse>({
+    query: DonationsQuery,
+    variables: {
+      orderBy: {
+        field,
+        direction: "desc",
+      },
+    },
+  });
+
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+
+  if (fetching || !data) return <p>Loading...</p>;
+
   return (
     <Box color="blue.700" mt={10} w="full">
-      <Heading fontWeight="black">LEADERBOARD</Heading>
-      <VStack spacing={5} mt={12}>
-        <LeaderboardItem
-          donation={{
-            displayName: "Bogdan",
-            count: 1000,
-            email: "test@test.ro",
-            createdAt: "2022-09-15T11:47:05.828Z",
-            team: "Team Name",
-            message: "Enjoy",
-          }}
-        />
-
-        <LeaderboardItem
-          donation={{
-            displayName: "Bogdan",
-            count: 1000,
-            email: "test@test.ro",
-            createdAt: "2022-09-15T11:47:05.828Z",
-            team: "Team Name",
-            message: "Enjoy",
-          }}
-        />
-        <LeaderboardItem
-          donation={{
-            displayName: "Bogdan",
-            count: 1000,
-            email: "test@test.ro",
-            createdAt: "2022-09-15T11:47:05.828Z",
-          }}
-        />
+      <VStack spacing={5}>
+        <Heading fontWeight="black" size="2xl">
+          LEADERBOARD
+        </Heading>
+        <RadioGroup onChange={setField} value={field}>
+          <HStack>
+            <Radio value="createdAt">Most recent</Radio>
+            <Radio value="count">Most pounds</Radio>
+          </HStack>
+        </RadioGroup>
+        <VStack spacing={5} mt={12} w="full">
+          {data.donations.map((donation) => (
+            <Flex
+              key={donation.id}
+              w="full"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <LeaderboardItem donation={donation} />
+            </Flex>
+          ))}
+        </VStack>
       </VStack>
     </Box>
   );
